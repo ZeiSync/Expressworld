a649f0cbfc1bb9ce2b0a8e13a12ca1662d19c3cd
Zeikai <zekapako29@gmail.com>
2020-05-08 16:31:55 +0700

    Upload and update avatar for users

--- middleware/pagination.middleware.js
+++ middleware/pagination.middleware.js
@@ -2,8 +2,8 @@
 
 module.exports.paginetedResut = (model) => {
   return (req, res, next) => {
-    const page = parseInt(req.query.page);
-    const limit = parseInt(req.query.limit);
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 2;
 
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
